104470760edfc26686eac0c4fb1acea6848c4042
Anwesh43 <anweshthecool0@gmail.com>
2021-06-08 02:09:11 +0530

    adding LUSNode class

--- index.ts
+++ index.ts
@@ -152,3 +152,46 @@ class Animator {
         }
     }
 }
+
+class LUSNode {
+
+    next : LUSNode 
+    prev : LUSNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LUSNode(this.i + 1)
+            this.next.prev = this  
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLUSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+
+    getNext(dir : number, cb : Function) : LUSNode {
+        var curr : LUSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
