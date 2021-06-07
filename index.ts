bcb4e10423124c5271ac6e636f5dbddb2c815175
Anwesh43 <anweshthecool0@gmail.com>
2021-06-08 02:13:41 +0530

    adding LineUpShooter class

--- index.ts
+++ index.ts
@@ -195,3 +195,26 @@ class LUSNode {
         return this 
     }
 }
+
+class LineUpShooter {
+
+    curr : LUSNode = new LUSNode(0)
+    dir : number = 1
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+}
